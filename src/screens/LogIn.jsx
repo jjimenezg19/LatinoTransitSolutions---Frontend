9923e2daedce517f8f6a83702a73bd1c713d43e0
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-09 12:59:34 -0600

    mejoras en login e implementacion de componente Input

--- src/screens/LogIn.jsx
+++ src/screens/LogIn.jsx
@@ -1,8 +1,10 @@
-import { AES } from "crypto-js"
 import { useState } from "react"
-import { useSystemStore } from "../stores/system.js"
+import { Link, useNavigate } from "react-router-dom"
+
+import Input from "../components/form/Input.jsx"
+
+import { AES } from "crypto-js"
 import { simpleAxios } from "../utils/axios.js"
-import { useNavigate } from "react-router-dom"
 
 const TOKEN_ENCRYPT = import.meta.env.VITE_TOKEN_ENCRYPT
 
@@ -15,12 +17,9 @@ export default function AboutUs() {
 
   const navigate = useNavigate()
 
-  const setIsAuth = useSystemStore((state) => state.setIsAuth)
-
   const login = () => {
     simpleAxios.post("/auth/login", { username: email, password }).then(({ ok, data }) => {
       if (ok) {
-        setIsAuth(true)
         localStorage.setItem("auth_token", AES.encrypt(data.token, TOKEN_ENCRYPT))
         navigate("/home")
       } else {
@@ -48,45 +47,25 @@ export default function AboutUs() {
   }
 
   return (
-    <section className="w-full h-full flex justify-center items-center">
-      <div className="flex flex-col gap-6 rounded-lg border shadow-sm mx-auto max-w-sm p-6">
-        <div className="flex flex-col gap-2">
+    <section className="w-full h-full flex items-center px-2">
+      <div className="flex flex-col gap-4 rounded-xl sm:border-2 shadow-md p-3 sm:p-6 max-w-100 m-auto">
+        <div className="flex flex-col gap-1">
           <h3 className="whitespace-nowrap tracking-tight text-2xl font-bold">Login</h3>
           <p className="text-slate-400">Enter your email below to login to your account</p>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <label className="font-medium leading-none" htmlFor="email">
-            Email
-          </label>
-          <div className="w-full">
-            <input onInput={({ target }) => setEmail(target.value)} className={errorEmail ? "border-red-500" : ""} placeholder="m@example.com" type="email" />
-            <small className="text-red-500 text-right h-6 block">{errorEmail}</small>
-          </div>
-        </div>
+        <div className="flex flex-col gap-1">
+          <Input label="Email" placeholder="m@example.com" error={errorEmail} onUpdateValue={setEmail} type="email"></Input>
+          <Input label="Password" placeholder="**********" error={errorPassword} onUpdateValue={setPassword} type="password"></Input>
 
-        <div className="flex flex-col gap-2">
-          <label className="font-medium leading-none" htmlFor="password">
-            Password
-          </label>
-          <div className="w-full">
-            <input onInput={({ target }) => setPassword(target.value)} className={errorPassword ? "border-red-500" : ""} placeholder="" type="password" />
-            <small className="text-red-500 text-right h-6 block">{errorPassword}</small>
-          </div>
-          <a className="ml-auto inline-block  underline" href="#">
-            Forgot your password?
-          </a>
+          {error ? <span className="text-red-500 text-center h-6 block">{error}</span> : null}
         </div>
-
-        {error ? <small className="text-red-500 text-right h-6 block">{error}</small> : null}
 
         <button onClick={validateFields}>Login</button>
 
-        <div className="w-full text-center ">
+        <div className="flex flex-col sm:flex-row justify-center gap-1 w-full text-center">
           <span>Don't have an account?</span>
-          <a className="underline ml-1" href="#">
-            Sign up
-          </a>
+          <Link to="/signup">Sign up</Link>
         </div>
       </div>
     </section>
