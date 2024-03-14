7484375e59e7f815f084f493b3bc1005f1d249fd
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-13 20:53:38 -0600

    improvements in FormActions Component

--- src/components/form/FormActions.jsx
+++ src/components/form/FormActions.jsx
@@ -1,18 +1,20 @@
 import Button from "./Button"
 
-export default function FormActions({ target, calcelText, isEditMode, onConfirm, onCancel }) {
+export default function FormActions({ target, loading, calcelText, disableCancel, disableConfirm, isEditMode, onConfirm, onCancel }) {
   return isEditMode ? (
     <div className="w-full flex justify-between">
-      <Button onClick={onCancel} color="negative">
+      <Button disabled={loading || disableCancel} onClick={onCancel} color="negative">
         {calcelText || "Cancel"}
       </Button>
-      <Button onClick={onConfirm} color="positive">
+      <Button disabled={loading || disableConfirm} onClick={onConfirm} color="positive">
         Update {target}
       </Button>
     </div>
   ) : (
     <div className="w-full flex justify-end">
-      <Button onClick={onConfirm}>Register {target}</Button>
+      <Button disabled={loading || disableConfirm} onClick={onConfirm}>
+        Register {target}
+      </Button>
     </div>
   )
 }
