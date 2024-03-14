176a99488ff88979293424968d9c58e35e86b5cd
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-13 20:49:12 -0600

    screen map elimination

--- src/router/SystemRouter.jsx
+++ src/router/SystemRouter.jsx
@@ -3,14 +3,12 @@ import { Routes, Route } from "react-router-dom"
 import App from "../layout/App.jsx"
 
 import Home from "../screens/Home.jsx"
-import GoogleMaps from "../screens/client/GoogleMaps.jsx"
 
 export default function SystemRouter() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="home" element={<Home />} />
-        <Route path="map" element={<GoogleMaps />} />
       </Route>
     </Routes>
   )
