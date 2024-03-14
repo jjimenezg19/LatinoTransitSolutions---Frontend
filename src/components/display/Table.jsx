3e0aa9e9cde8ba6263a1c0c2440a68678c83ff62
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-13 20:54:05 -0600

    improvements in table and button components

--- src/components/display/Table.jsx
+++ src/components/display/Table.jsx
@@ -1,11 +1,11 @@
 import Button from "../form/Button"
 
-export default function Table({ heads, data, actions, className, onUpdate, onDelete, onGoto }) {
+export default function Table({ heads, data, actions, className, onUpdate, onDelete, onGoto, onClickRow }) {
   actions = actions || ["update", "delete", "goto"]
 
   return (
-    <div className={className + " relative overflow-x-auto shadow-md md:rounded-lg h-fit"}>
-      <table className="w-full text-sm text-left rtl:text-right text-word">
+    <div className={className + " relative overflow-x-auto h-fit"}>
+      <table className="w-full text-base text-center text-word-100 shadow-md md:rounded-lg overflow-hidden">
         <thead className="text-xs uppercase bg-secondary-200 sticky top-0">
           <tr>
             {heads.map((head, index) => (
@@ -16,9 +16,9 @@ export default function Table({ heads, data, actions, className, onUpdate, onDel
           </tr>
         </thead>
 
-        <tbody>
+        <tbody className="h-full overflow-y-auto">
           {data.map((row, index) => (
-            <tr key={"body_row_" + index} className="bg-secondary-100 hover:bg-secondary-200/70">
+            <tr onClick={() => (onClickRow ? onClickRow({ index, row }) : null)} key={"body_row_" + index} className={`${onClickRow ? "cursor-pointer" : ""} bg-secondary-100 hover:bg-secondary-200/70`}>
               {heads
                 .filter((head) => head.scope)
                 .map((head, index) => (
@@ -26,19 +26,19 @@ export default function Table({ heads, data, actions, className, onUpdate, onDel
                     {row[head.scope]}
                   </td>
                 ))}
-              <td scope="row" className="flex px-3 py-2 justify-center gap-1">
+              <td scope="row" className="flex px-3 py-2 justify-center gap-2">
                 {actions?.includes("update") ? (
-                  <Button onClick={() => onUpdate({ index, row })} type="outlined" size="sm">
+                  <Button onClick={() => onUpdate({ index, row })} type="outlined" size="xs" color="positive">
                     <i className="fas fa-pen"></i>
                   </Button>
                 ) : null}
                 {actions?.includes("delete") ? (
-                  <Button onClick={() => onDelete({ index, row })} type="outlined" size="sm">
+                  <Button onClick={() => onDelete({ index, row })} type="outlined" size="xs" color="negative">
                     <i className="fas fa-trash"></i>
                   </Button>
                 ) : null}
                 {actions?.includes("goto") ? (
-                  <Button onClick={() => onGoto({ index, row })} type="outlined" size="sm">
+                  <Button onClick={() => onGoto({ index, row })} type="outlined" size="xs">
                     <i className="fas fa-share"></i>
                   </Button>
                 ) : null}
--- src/components/form/Button.jsx
+++ src/components/form/Button.jsx
@@ -1,22 +1,23 @@
 export default function Button({ size, type, color, disabled, children, onClick }) {
   size = size || "md"
-  type = type || "regular"
+  type = disabled ? "disabled" : type || "regular"
   color = color || "primary"
 
   const buttonSizes = {
-    sm: "!h-8 !px-2 !text-sm",
-    md: "!h-10 !px-3 !text-base"
+    xs: "h-6 px-1 text-xs",
+    sm: "h-6 px-1 text-xs md:h-8 md:px-2 md:text-sm",
+    md: "h-8 px-2 text-sm md:h-10 md:px-3 md:text-base"
   }
 
   const buttonColors = {
     regular: `bg-${color}-100 border-transparent hover:bg-${color}-200 active:bg-${color}-300 text-word-100`,
     outlined: `bg-trasparent border-${color}-100 text-${color}-100 hover:border-${color}-200 hover:text-${color}-200 active:border-${color}-300 active:text-${color}-300`,
     flat: `bg-transparent border-transparent text-${color}-100 hover:bg-${color}-100/30 active:bg-${color}-100/20`,
-    disabled: `bg-${color}-100/50 border-transparent text-word-100/50 cursor-not-allowed`
+    disabled: `bg-${color}-100/50 border-transparent text-word-100/50 !cursor-not-allowed`
   }
 
   return (
-    <button disabled={disabled} onClick={onClick} className={`${buttonSizes[size]} ${!disabled ? buttonColors[type] : buttonColors["disabled"]} cursor-pointer rounded-md border-2`}>
+    <button disabled={disabled} onClick={onClick} className={`${buttonSizes[size]} ${buttonColors[type]} cursor-pointer rounded-md border-2`}>
       {children}
     </button>
   )
