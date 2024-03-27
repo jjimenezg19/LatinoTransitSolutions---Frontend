56b4758214de0fe39db483404448f53a67411fd4
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-27 13:58:58 -0600

    input number improvements

--- src/components/form/InputNumber.jsx
+++ src/components/form/InputNumber.jsx
@@ -2,19 +2,32 @@ import Button from "./Button"
 import Input from "./Input"
 
 export default function InputNumber({ label, value, disabled, readonly, placeholder, error, min, max, onUpdateValue }) {
+  min = min ? Number(min) : null
+  max = max ? Number(max) : null
+
+  let interval = null
+
+  if (min !== null && value < min) {
+    value = min
+    onUpdateValue(value)
+  }
+
+  if (max !== null && value > max) {
+    value = max
+    onUpdateValue(value)
+  }
+
   const handleInput = (val) => {
     if (disabled || readonly) return null
 
     val = Number(val.replace(/[^\d]/g, ""))
 
-    if (val < Number(min)) {
-      onUpdateValue(Number(min))
-      return null
+    if (min !== null && val < min) {
+      val = min
     }
 
-    if (val > Number(max)) {
-      onUpdateValue(Number(max))
-      return null
+    if (max !== null && val > max) {
+      val = max
     }
 
     onUpdateValue(val)
@@ -25,11 +38,11 @@ export default function InputNumber({ label, value, disabled, readonly, placehol
 
     const newval = value + action
 
-    if (newval < Number(min)) {
+    if (min !== null && newval < min) {
       return null
     }
 
-    if (newval > Number(max)) {
+    if (max !== null && newval > max) {
       return null
     }
 
@@ -41,11 +54,11 @@ export default function InputNumber({ label, value, disabled, readonly, placehol
       <label className="font-medium leading-none">{label}</label>
       <div className="w-full">
         <div className="w-full flex items-center">
-          <Button disabled={disabled} readonly={readonly} onClick={() => handleButton(-1)} className="!pl-2 !pr-4 -mr-2">
+          <Button disabled={disabled || (min !== null && value <= min)} readonly={readonly} onClick={() => handleButton(-1)} className="!pl-2 !pr-4 -mr-2">
             <i className="fas fa-minus"></i>
           </Button>
           <Input value={value} onUpdateValue={handleInput} className="w-full relative" type="text" disabled={disabled} readonly={readonly} error={error} placeholder={placeholder} noHint></Input>
-          <Button disabled={disabled} readonly={readonly} onClick={() => handleButton(1)} className="!pr-2 !pl-4 -ml-2">
+          <Button disabled={disabled || (max !== null && value >= max)} readonly={readonly} onClick={() => handleButton(1)} className="!pr-2 !pl-4 -ml-2">
             <i className="fas fa-plus"></i>
           </Button>
         </div>
